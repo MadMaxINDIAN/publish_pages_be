f43cd33d9094beb65ea372d52a65d4cb559c7f15
Samyak Shah <samyakshah123@gmail.com>
2021-11-01 12:36:33 +0530

    Added authorization middlewares

---
+++ middleware/userAuth.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.get("Authorization");
+  let decodedToken;
+  try {
+    decodedToken = jwt.decode(token);
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const err = new Error("Unauthorized");
+    err.statusCode = 401;
+    throw err;
+  }
+  User.findOne({ uid: decodedToken.user_id })
+    .then((user) => {
+      if (user && user.type !== "user") {
+        const err = new Error("Not authorized");
+        err.statusCode = 401;
+        throw err;
+      } else {
+        req.user = decodedToken;
+        next();
+      }
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+        next(err);
+      }
+    });
+};
--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { addUser, addAdminUser, getAllUsers } = require("../controllers/user");
-const isAuth = require("../middleware/auth");
+const adminAuth = require("../middleware/adminAuth");
+const userAuth = require("../middleware/userAuth");
 
 // Register Different User
 router.post("/register", addUser);
-router.post("/admin/register", addAdminUser);
+router.post("/admin/register", adminAuth, addAdminUser);
 
 // Get all users for admin panel
-router.get("/admin/users", getAllUsers);
+router.get("/admin/users", adminAuth, getAllUsers);
 module.exports = router;
