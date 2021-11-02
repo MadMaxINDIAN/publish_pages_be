b4c32e8caa8514234439acd619f796fbc28950f8
Naman Khater <naman22khater@gmail.com>
2021-11-02 11:54:41 +0530

    create book image added

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { updateBook, createBook, getBook, deleteBook } = require('../controllers/book');
 const router = express.Router();
+const ImageUploader = require("../multer/upload.controller");
 
 router.get("/", getBook);
-router.post("/", createBook);
+router.post("/", ImageUploader.single('frontCoverImage'), createBook);
 router.patch("/", updateBook);
 router.delete("/", deleteBook);
 
