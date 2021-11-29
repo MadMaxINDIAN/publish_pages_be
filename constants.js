59964fee077c2a430a7420dbef8a49e5a5dbc7ed
Naman Khater <naman22khater@gmail.com>
2021-11-30 00:20:35 +0530

    updated categories

--- constants.js
+++ constants.js
@@ -1,7 +1,25 @@
-const languages = ['English', 'Hindi', 'Other']
-const categories = ['Fiction', 'Non-Fiction', 'Children', 'Cooking', 'History', 'Biography', 'Poetry', 'Science', 'Art', 'Religion', 'Travel', 'Other']
-
+const languages = ["English", "Hindi", "Other"];
+const categories = [
+  "Fiction",
+  "Non-Fiction",
+  "Finance",
+  "Business",
+  "Education",
+  "Health",
+  "Lifestyle",
+  "Religion",
+  "Science",
+  "Technology",
+  "Children",
+  "Cooking",
+  "History",
+  "Biography",
+  "Poetry",
+  "Art",
+  "Travel",
+  "Other",
+];
 module.exports = {
-    languages,
-    categories
-}
+  languages,
+  categories,
+};
